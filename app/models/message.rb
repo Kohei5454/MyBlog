eb4307b070d0a9f4939b8dbae1c26e88af1e203f
Kohei <kou.program54@gmail.com>
2017-07-29 06:58:22 +0000

    new-column

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ActiveRecord::Base
     validates :title , length: { maximum: 32 } , presence: true
     validates :message , length: { maximum: 256 } , presence: true
     validates :name , length: { maximum: 32 }
+    validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 125 }
 end
